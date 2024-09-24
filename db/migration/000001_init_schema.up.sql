0601fa192cf53db2d676860fb8c01134de0b83fd
Davi <daviregonatti@gmail.com>
2024-09-24 10:53:38 -0300

    fix: fixed timestamp attr on entry table
    changed the created at prop to not null

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -9,7 +9,7 @@ CREATE TABLE "entries" (
     "id" bigserial PRIMARY KEY,
     "account_id" bigint NOT NULL,
     "amount" bigint NOT NULL,
-    "created_at" timestamptz DEFAULT (now())
+    "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 CREATE TABLE "transfers" (
     "id" bigserial PRIMARY KEY,

dbd5467f4a706735fce266948a1febab95eba1bf
Davi <daviregonatti@gmail.com>
2024-09-30 13:26:17 -0300

    feat: patch and delete account reqs
    created patch and delete account requests. Using ID from URI and balance from JSON body

--- api/account.go
+++ api/account.go
@@ -81,6 +81,70 @@ func (server *Server) listAccount(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
-	
+
 	ctx.JSON(http.StatusOK, accs)
+}
+
+type updateAccountRequestUri struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+type updateAccountRequestJSON struct {
+	Balance int64 `json:"balance" binding:"required"`
+}
+
+func (server *Server) updateAccount(ctx *gin.Context) {
+	var reqUri updateAccountRequestUri
+	if err := ctx.ShouldBindUri(&reqUri); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	var reqJson updateAccountRequestJSON
+	if err := ctx.ShouldBindJSON(&reqJson); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	} 
+
+	arg := db.UpdateAccountParams{
+		ID: reqUri.ID,
+		Balance: reqJson.Balance,
+	}
+	acc, err := server.store.UpdateAccount(ctx, arg)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, acc)
+
+}
+
+type deleteAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+// TODO: to delete account, it needs to fix the constraints first (no idea how)
+func (server *Server) deleteAccount(ctx *gin.Context) {
+	var req deleteAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	err := server.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, nil)
 }

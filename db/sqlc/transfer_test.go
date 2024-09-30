17b568c0e8d29ff0d583f0886c7c11aae68c7275
Davi <daviregonatti@gmail.com>
2024-09-29 23:29:03 -0300

    update:  del updading acc balances
    removed changing account's balances in the transfer test func

--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -18,7 +18,7 @@ func createRandomTransfer(t *testing.T, accounts ...Account) Transfer {
 	if len(accounts) == 0 {
 		acc1 = createRandomAccount(t)
 		acc2 = createRandomAccount(t)
-	}else {
+	} else {
 		acc1 = accounts[0]
 		acc2 = accounts[1]
 	}
@@ -30,35 +30,37 @@ func createRandomTransfer(t *testing.T, accounts ...Account) Transfer {
 		ToAccountID:   acc2.ID,
 		Amount:        transferAmount,
 	}
-	
+
 	// creating and testing transfer obj
 	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
 
 	require.NoError(t, err)
 	require.NotEmpty(t, transfer)
-	
+
 	require.NotZero(t, transfer.ID)
 	require.NotZero(t, transfer.CreatedAt)
 
-	require.Equal(t, acc1.ID, transfer.FromAccountID) 
-	require.Equal(t, acc2.ID, transfer.ToAccountID) 
-	
-	//withdrawing from FromAccount
-	argFrom := UpdateAccountParams{
-		ID: acc1.ID,
-		Balance: acc1.Balance - transferAmount,
-	}
+	require.Equal(t, acc1.ID, transfer.FromAccountID)
+	require.Equal(t, acc2.ID, transfer.ToAccountID)
 
-	// receiving amount to ToAccount
-	argTo := UpdateAccountParams{
-		ID: acc2.ID,
-		Balance: acc2.Balance + transferAmount,
-	}
-	errFromAcc := testQueries.UpdateAccount(context.Background(), argFrom)
-	errToAcc := testQueries.UpdateAccount(context.Background(), argTo)
+	// UNNEEDED CODE
 
-	require.NoError(t, errFromAcc)
-	require.NoError(t, errToAcc)
+	// //withdrawing from FromAccount
+	// argFrom := UpdateAccountParams{
+	// 	ID: acc1.ID,
+	// 	Balance: acc1.Balance - transferAmount,
+	// }
+
+	// // receiving amount to ToAccount
+	// argTo := UpdateAccountParams{
+	// 	ID: acc2.ID,
+	// 	Balance: acc2.Balance + transferAmount,
+	// }
+	// _, errFromAcc := testQueries.UpdateAccount(context.Background(), argFrom)
+	// _, errToAcc := testQueries.UpdateAccount(context.Background(), argTo)
+
+	// require.NoError(t, errFromAcc)
+	// require.NoError(t, errToAcc)
 
 	return transfer
 }
@@ -66,11 +68,11 @@ func createRandomTransfer(t *testing.T, accounts ...Account) Transfer {
 func TestCreateTransfer(t *testing.T) {
 	// acc1 := createRandomAccount(t)
 	// acc2 := createRandomAccount(t)
-	
+
 	createRandomTransfer(t)
 }
 
-func TestGetTransfer(t *testing.T){
+func TestGetTransfer(t *testing.T) {
 	transfer1 := createRandomTransfer(t)
 
 	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
@@ -86,7 +88,7 @@ func TestGetTransfer(t *testing.T){
 
 }
 
-func TestListTranfers(t *testing.T){
+func TestListTranfers(t *testing.T) {
 	acc1 := createRandomAccount(t)
 	acc2 := createRandomAccount(t)
 
@@ -96,9 +98,9 @@ func TestListTranfers(t *testing.T){
 
 	arg := ListTransfersParams{
 		FromAccountID: acc1.ID,
-		ToAccountID: acc2.ID,
-		Limit: 5,
-		Offset: 5,
+		ToAccountID:   acc2.ID,
+		Limit:         5,
+		Offset:        5,
 	}
 
 	transfers, err := testQueries.ListTransfers(context.Background(), arg)
@@ -106,7 +108,7 @@ func TestListTranfers(t *testing.T){
 	require.NoError(t, err)
 	require.Len(t, transfers, 5)
 
-	for _, tranf := range transfers{
+	for _, tranf := range transfers {
 		require.NotEmpty(t, tranf)
 	}
 }

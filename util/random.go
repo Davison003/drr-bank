29c0dd53331083ce8fc3649e54c924382d303ea9
Davi <daviregonatti@gmail.com>
2024-09-30 16:16:16 -0300

    feat: load config file func

--- util/random.go
+++ util/random.go
@@ -13,8 +13,8 @@ func init() {
 }
 
 // generates random int min < x < max
-func RandomInt(min, max int64) int64{
-	return min + rand.Int63n(max - min + 1)
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
 
 }
 
@@ -22,14 +22,13 @@ func RandomString(n int) string {
 	var sb strings.Builder
 	k := len(alphabet)
 
-	for i:= 0; i < n; i++{
-		c:= alphabet[rand.Intn(k)]
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
 		sb.WriteByte(c)
 	}
 
 	return sb.String()
 }
-
 
 func RandomOwner() string {
 	return RandomString(7)
@@ -41,7 +40,7 @@ func RandomMoney() int64 {
 
 func RandomCurrency() string {
 	currencies := []string{"EUR", "USD", "BRL"}
-	n:= len(currencies)
+	n := len(currencies)
 
 	return currencies[rand.Intn(n)]
 }
